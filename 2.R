a95c6f7cf6d00691cd692eab9f3269700cde7ee3
Daud Dhiya <daud.dhiya31@gmail.com>
2023-05-03 00:57:42 +0700

    fix: answer number 2

---
+++ 2.R
@@ -0,0 +1,42 @@
+# Poin A
+# Jawaban ada di README.md
+
+lambda <- 1.8
+x <- 4
+
+# Poin B
+probability <- dpois(x=x,lambda=lambda)
+probability
+
+# Poin C
+probability <- ppois(q=x, lambda=lambda, lower.tail=TRUE)
+probability
+
+# Poin D
+probability <- ppois(q=x+1, lambda=lambda, lower.tail=FALSE)
+probability
+
+# Poin E
+nilai_harapan <- lambda
+nilai_harapan
+
+standar_deviasi <- sqrt(nilai_harapan)
+standar_deviasi
+
+# Poin F
+arr <- 0:6
+plot(arr, dpois(arr, lambda=1.8),
+     type='h',
+     main='Histogram Distribusi Poisson',
+     xlab='Banyak kematian'
+     ylab='Probabilitas',
+)
+
+# Poin G
+n <- 10
+set.seed(1)
+ans <- rpois(n=n, lambda=1.8)
+ans
+
+# Poin H
+# Jawaban ada di README.md
