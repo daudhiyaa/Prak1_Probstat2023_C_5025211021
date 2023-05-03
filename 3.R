61325e82902ca40efca9d6086cffcb69ab3acf98
Daud Dhiya <daud.dhiya31@gmail.com>
2023-05-03 18:38:03 +0700

    fix: answer number 3

--- 3.R
+++ 3.R
@@ -2,16 +2,22 @@ x <- 3
 v <- 10
 
 # Poin A
-probability <- dchisq(x, 10)
+probability <- dchisq(x, v)
 probability
 
 # Poin B
 # set.seed(1)
 n <- 500
-hist(rchisq(n, v), xlab = "X", ylab = "V", main = "Grafik Histogram")
+hist(
+  rchisq(n, v), 
+  xlab = "X", 
+  ylab = "V", 
+  main = "Grafik Histogram"
+)
 
 # Poin C
 mean <- v
-variance <- 2 * v
 mean
+
+variance <- 2 * v
 variance
